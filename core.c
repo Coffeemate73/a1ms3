425d696f2219346ca65a28964810b57b57d4f28e
Coffeemate73 <102769743+Coffeemate73@users.noreply.github.com>
2022-04-05 17:22:54 -0400

    Create core.c

---
+++ core.c
@@ -0,0 +1,45 @@
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include "core.h"
+
+//////////////////////////////////////
+// USER INTERFACE FUNCTIONS
+//////////////////////////////////////
+
+// Clear the standard input buffer
+void clearInputBuffer(void)
+{
+    // Discard all remaining char's from the standard input buffer:
+    while (getchar() != '\n')
+    {
+        ; // do nothing!
+    }
+}
+
+// Wait for user to input the "enter" key to continue
+void suspend(void)
+{
+    printf("<ENTER> to continue...");
+    clearInputBuffer();
+    putchar('\n');
+}
+
+
+//////////////////////////////////////
+// USER INPUT FUNCTIONS
+//////////////////////////////////////
+
+//
+// Copy your work done from Milestone #2 (core.c) into this file
+// - Organize your functions in the same order as they are listed in the core.h file
+//
+
+
+
+//////////////////////////////////////
+// UTILITY FUNCTIONS
+//////////////////////////////////////
